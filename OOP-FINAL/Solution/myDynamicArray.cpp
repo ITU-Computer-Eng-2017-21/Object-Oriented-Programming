1c9182d7b4cd92683279fbf02a428e00f51ffb43
incessy <barisincesu@outlook.com>
2020-07-14 22:56:49 +0300

    question1 solved.

--- OOP-FINAL/Solution/myDynamicArray.cpp
+++ OOP-FINAL/Solution/myDynamicArray.cpp
@@ -1,3 +1,82 @@
 #include "myDynamicArray.h"
 
 using namespace std;
+
+template <class type>
+int myDynamicArray<type>::getsize(){
+    return size;
+}
+
+template <class type>
+int myDynamicArray<type>::getcapacity(){
+    return capacity;
+}
+template <class type>
+type* myDynamicArray<type>::gethead(){
+    return DynamicArray;
+}
+
+template <class type>
+void myDynamicArray<type>::add(type input){
+    if (size < capacity){
+        DynamicArray[size] = input;
+        size++;
+    }
+    else{
+        capacity = capacity * 2;
+        type * newarray = new type[capacity];
+        for (int i = 0; i < size; i++)
+        {
+            newarray[i] = DynamicArray[i];
+        }
+
+        delete [] DynamicArray;
+        DynamicArray = newarray;
+        DynamicArray[size] = input;
+        size++;
+        cout << "array resized " << capacity << endl;
+    }
+}
+
+template <class type>
+type myDynamicArray<type>::remove(){
+    if (size <= capacity/2)
+    {
+        capacity = capacity / 2;
+        type * newarray = new type[capacity];
+        for (int i = 0; i < size; i++)
+        {
+            newarray[i] = DynamicArray[i];
+        }
+
+        //delete [] DynamicArray;
+        DynamicArray = newarray;
+        type abc = DynamicArray[size-1];
+        DynamicArray[size-1] = 0;
+        size--;
+        cout << "array resized " << capacity << endl;
+        return abc;
+    }
+    else
+    {
+    	type abc = DynamicArray[size-1];
+        DynamicArray[size-1] = 0;
+        size--;
+        return abc;
+    }
+    
+}
+
+template <class type>
+myDynamicArray<type>::myDynamicArray(){
+    capacity = 5;
+    size = 0;
+    DynamicArray = new type[capacity];
+    cout << "new array of capacity=" << capacity <<  " has been created" << endl;
+}
+
+template <class type>
+myDynamicArray<type>::~myDynamicArray(){
+    delete[] DynamicArray;
+    cout << "a dynamic array has been destroyed" << endl;
+}
--- OOP-FINAL/Solution/myDynamicArray.h
+++ OOP-FINAL/Solution/myDynamicArray.h
@@ -1,2 +1,22 @@
 #pragma once
 #include <iostream>
+
+template <class type>
+
+class myDynamicArray
+{
+public:
+    int size;
+    int capacity;
+    type * DynamicArray;
+
+    int getsize();
+    int getcapacity();
+    type* gethead();
+    
+    void add( type );
+    type remove();
+    
+    myDynamicArray();
+    ~myDynamicArray();
+};
